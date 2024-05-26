26dead2ea47f40230eec80dcdef544ea217236f3
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-27 01:34:05 +0200

    New: Enhance through adding base, headings, images, and inline elements to `typographyStyles`

--- styles/typography.ts
+++ styles/typography.ts
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT 4️⃣
 import {type PluginUtils} from "tailwindcss/types/config"
 
 const typographyStyles = function typographyStyles({theme}: PluginUtils) {
@@ -67,7 +67,90 @@ const typographyStyles = function typographyStyles({theme}: PluginUtils) {
         "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 0.4)",
         "--tw-prose-invert-pre-border": theme("colors.zinc.200 / 0.1"),
         "--tw-prose-invert-th-borders": theme("colors.zinc.700"),
-        "--tw-prose-invert-td-borders": theme("colors.zinc.800")
+        "--tw-prose-invert-td-borders": theme("colors.zinc.800"),
+
+        // BASE
+        "lineHeight": theme("lineHeight.7"),
+        "color": "var(--tw-prose-body)",
+
+        "> *": {
+          marginTop: theme("spacing.10"),
+          marginBottom: theme("spacing.10")
+        },
+
+        "p": {
+          marginTop: theme("spacing.7"),
+          marginBottom: theme("spacing.7")
+        },
+
+        // HEADINGS
+        "h2, h3": {
+          fontWeight: theme("fontWeight.semi-bold"),
+          color: "var(--tw-prose-headings)"
+        },
+
+        "h2": {
+          fontSize: theme("fontSize.xl")[0],
+          lineHeight: theme("lineHeight.7"),
+          marginTop: theme("spacing.20"),
+          marginBottom: theme("spacing.4")
+        },
+
+        "h3": {
+          fontSize: theme("fontSize.base")[0],
+          lineHeight: theme("lineHeight.7"),
+          marginTop: theme("spacing.16"),
+          marginBottom: theme("spacing.4")
+        },
+
+        "is(h2, h3) + *": {
+          marginTop: 0
+        },
+
+        // IMAGES
+        "img": {
+          borderRadius: theme("borderRadius.xl-3")
+        },
+
+        // INLINE ELEMENTS
+        "a": {
+          fontWeight: theme("fontWeight.semi-bold"),
+          textDecoration: "underline",
+          textDecorationColor: "var(--tw-prose-underline)",
+          transitionProperty: "color, text-decoration-color",
+          transitionDuration: theme("transitionDuration.150"),
+          transitionTimingFunction: theme("transitionTimingFunction.in-out"),
+          color: "var(--tw-prose-links)"
+        },
+
+        "a:hover": {
+          textDecorationColor: "var(--tw-prose-underline-hover)",
+          color: "var(--tw-prose-links-hover)"
+        },
+
+        "strong": {
+          fontWeight: theme("fontWeight.semi-bold"),
+          color: "var(--tw-prose-bold)"
+        },
+
+        "code": {
+          display: "inline-block",
+          fontSize: theme("fontSize.sm")[0],
+          fontWeight: theme("fontWeight.semi-bold"),
+          color: "var(--tw-prose-code)",
+          backgroundColor: "var(--tw-prose-code-bg)",
+          paddingLeft: theme("spacing.1"),
+          paddingRight: theme("spacing.1"),
+          borderRadius: theme("borderRadius.lg")
+        },
+
+        ":is(h2, h3) code": {
+          fontWeight: theme("fontWeight.bold")
+        },
+
+        "a code": {
+          color: "inherit"
+        }
       }
     }
   }
