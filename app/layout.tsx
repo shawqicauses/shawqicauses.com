f1696f3d4279d989876f37f045636742f008aa41
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-28 01:53:50 +0200

    New: Enhance through adding `MobileNavigation` to `Header`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT 4️⃣
 import type {Metadata} from "next"
-import {Poppins} from "next/font/google"
 import {PropsWithChildren} from "react"
 import {Layout} from "../components/index"
 import "../styles/global.css"
 import Providers from "./providers"
-
-const poppins = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
-})
 
 export const metadata: Metadata = {
   title: {
@@ -28,7 +22,7 @@ export const metadata: Metadata = {
 const RootLayout = function RootLayout({children}: PropsWithChildren) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <body className={`${poppins.className} flex h-full bg-background-layer-2`}>
+      <body className="flex h-full bg-background-layer-2">
         <Providers>
           <div className="flex w-full">
             <Layout>{children}</Layout>
--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,15 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT 3️⃣
+// DONE REVIEWING: GITHUB COMMIT 4️⃣
 
+import {
+  Popover,
+  PopoverButton,
+  PopoverOverlay,
+  PopoverPanel,
+  Transition,
+  TransitionChild
+} from "@headlessui/react"
 import Link from "next/link"
 import {usePathname} from "next/navigation"
 import {ComponentPropsWithoutRef, PropsWithChildren} from "react"
@@ -10,6 +18,93 @@ import {cn} from "../lib/utils"
 interface NavigationItemProps extends PropsWithChildren {
   href: string
 }
+
+const navigations = [
+  {name: "About", href: "/about"},
+  {name: "Articles", href: "/articles"},
+  {name: "Projects", href: "/projects"},
+  {name: "Speakings", href: "/speakings"},
+  {name: "Uses", href: "/uses"}
+]
+
+// MOBILE NAVIGATION
+const ChevronDownIcon = function ChevronDownIcon(props: ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 8 6" aria-hidden="true" {...props}>
+      <path
+        fill="none"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M1.75 1.75 4 4.25l2.25-2.5"
+      />
+    </svg>
+  )
+}
+
+const CloseIcon = function CloseIcon(props: ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m17.25 6.75-10.5 10.5M6.75 6.75l10.5 10.5"
+      />
+    </svg>
+  )
+}
+
+const MobileNavigation = function MobileNavigation(
+  props: ComponentPropsWithoutRef<typeof Popover>
+) {
+  return (
+    <Popover {...props}>
+      <PopoverButton className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+        Menu
+        <ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
+      </PopoverButton>
+      <Transition>
+        <TransitionChild
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0">
+          <PopoverOverlay className="fixed inset-0 bg-zinc-800/40 backdrop-blur-sm dark:bg-black/80" />
+        </TransitionChild>
+        <TransitionChild
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-150 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95">
+          <PopoverPanel
+            focus
+            className="fixed inset-x-4 top-8 z-50 origin-top rounded-xl-3 bg-white p-8 ring-1 ring-zinc-900/5 dark:bg-zinc-900 dark:ring-zinc-800">
+            <div className="flex flex-row-reverse items-center justify-between">
+              <PopoverButton aria-label="Close Menu" className="-m-1 p-1">
+                <CloseIcon className="h-6 w-6 text-zinc-500 dark:text-zinc-400" />
+              </PopoverButton>
+              <h2 className="text-sm font-medium text-zinc-600 dark:text-zinc-400">Navigation</h2>
+            </div>
+            <nav className="mt-6">
+              <ul className="-my-2 divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+                Navigation Item
+              </ul>
+            </nav>
+          </PopoverPanel>
+        </TransitionChild>
+      </Transition>
+    </Popover>
+  )
+}
+
+// DESKTOP NAVIGATION
 
 const NavigationItem = function NavigationItem({href, children}: NavigationItemProps) {
   const isActive = usePathname() === href
@@ -30,14 +125,6 @@ const NavigationItem = function NavigationItem({href, children}: NavigationItemP
   )
 }
 
-const navigations = [
-  {name: "About", href: "/about"},
-  {name: "Articles", href: "/articles"},
-  {name: "Projects", href: "/projects"},
-  {name: "Speakings", href: "/speakings"},
-  {name: "Uses", href: "/uses"}
-]
-
 const DesktopNavigation = function DesktopNavigation(props: ComponentPropsWithoutRef<"nav">) {
   return (
     <nav {...props}>
@@ -55,7 +142,7 @@ const DesktopNavigation = function DesktopNavigation(props: ComponentPropsWithou
 const Header = function Header() {
   return (
     <header>
-      <DesktopNavigation />
+      <MobileNavigation />
     </header>
   )
 }
