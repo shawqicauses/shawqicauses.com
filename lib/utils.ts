6c6c7a62452257bbf7f720b23a3699fdff85b96c
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-28 02:47:57 +0200

    New: Enhance through adding `clamp` to `utils.ts` file

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 import {clsx, type ClassValue} from "clsx"
 import {twMerge} from "tailwind-merge"
 
@@ -6,6 +6,12 @@ export const cn = function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-const utils = {cn}
+export const clamp = function clamp(number: number, a: number, b: number) {
+  const min = Math.min(a, b)
+  const max = Math.max(a, b)
+  return Math.min(Math.max(number, min), max)
+}
+
+const utils = {cn, clamp}
 
 export default utils
