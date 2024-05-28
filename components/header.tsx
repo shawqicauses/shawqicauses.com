df211f3cb9b751dab0dc506edb77c3689ecc71f2
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-28 02:17:51 +0200

    New: Enhance through adding `SunIcon` and `MoonIcon` to `Header`

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT 5️⃣
+// DONE REVIEWING: GITHUB COMMIT 6️⃣
 
 import {
   Popover,
@@ -119,7 +119,6 @@ const MobileNavigation = function MobileNavigation(
 }
 
 // DESKTOP NAVIGATION
-
 const NavigationItem = function NavigationItem({href, children}: NavigationItemProps) {
   const isActive = usePathname() === href
   return (
@@ -150,6 +149,38 @@ const DesktopNavigation = function DesktopNavigation(props: ComponentPropsWithou
         ))}
       </ul>
     </nav>
+  )
+}
+
+// THEME
+const SunIcon = function SunIcon(props: ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      aria-hidden="true"
+      {...props}>
+      <path d="M8 12.25A4.25 4.25 0 0 1 12.25 8v0a4.25 4.25 0 0 1 4.25 4.25v0a4.25 4.25 0 0 1-4.25 4.25v0A4.25 4.25 0 0 1 8 12.25v0Z" />
+      <path
+        fill="none"
+        d="M12.25 3v1.5M21.5 12.25H20M18.791 18.791l-1.06-1.06M18.791 5.709l-1.06 1.06M12.25 20v1.5M4.5 12.25H3M6.77 6.77 5.709 5.709M6.77 17.73l-1.061 1.061"
+      />
+    </svg>
+  )
+}
+
+const MoonIcon = function MoonIcon(props: ComponentPropsWithoutRef<"svg">) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        strokeWidth={1.5}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M17.25 16.22a6.937 6.937 0 0 1-9.47-9.47 7.451 7.451 0 1 0 9.47 9.47ZM12.75 7C17 7 17 2.75 17 2.75S17 7 21.25 7C17 7 17 11.25 17 11.25S17 7 12.75 7Z"
+      />
+    </svg>
   )
 }
 
