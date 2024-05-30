2e875cd07246b175a440480b2aa5dbfbfd6450ba
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-31 00:46:11 +0200

    New: Enhance through adding content offset `div` to `Header`

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT 1️⃣8️⃣
+// DONE REVIEWING: GITHUB COMMIT 1️⃣9️⃣
 
 import {
   Popover,
@@ -347,67 +347,70 @@ const Header = function Header() {
   }, [isHomePage])
 
   return (
-    <header
-      className="pointer-events-none relative z-50 flex flex-none flex-col"
-      style={{height: "var(--header-height", marginBottom: "var(--header-mb)"}}>
-      {isHomePage && (
-        <Fragment>
-          <div
-            ref={avatarRef}
-            className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
-          />
-          <Container
-            className="top-0 order-last -mb-3 pt-3"
-            style={{position: "var(--header-position)" as CSSProperties["position"]}}>
+    <Fragment>
+      <header
+        className="pointer-events-none relative z-50 flex flex-none flex-col"
+        style={{height: "var(--header-height", marginBottom: "var(--header-mb)"}}>
+        {isHomePage && (
+          <Fragment>
             <div
-              className="top-[var(--avatar-top,theme(spacing.3))] w-full"
-              style={{position: "var(--header-inner-position)" as CSSProperties["position"]}}>
-              <div className="relative">
-                <AvatarContainer
-                  className="absolute left-0 top-3 origin-left transition-opacity"
-                  style={{
-                    opacity: "var(--avatar-border-opacity, 0)",
-                    transform: "var(--avatar-border-transform)"
-                  }}
-                />
-                <Avatar
-                  large
-                  className="block h-16 w-16 origin-left"
-                  style={{transform: "var(--avatar-image-transform)"}}
-                />
+              ref={avatarRef}
+              className="order-last mt-[calc(theme(spacing.16)-theme(spacing.3))]"
+            />
+            <Container
+              className="top-0 order-last -mb-3 pt-3"
+              style={{position: "var(--header-position)" as CSSProperties["position"]}}>
+              <div
+                className="top-[var(--avatar-top,theme(spacing.3))] w-full"
+                style={{position: "var(--header-inner-position)" as CSSProperties["position"]}}>
+                <div className="relative">
+                  <AvatarContainer
+                    className="absolute left-0 top-3 origin-left transition-opacity"
+                    style={{
+                      opacity: "var(--avatar-border-opacity, 0)",
+                      transform: "var(--avatar-border-transform)"
+                    }}
+                  />
+                  <Avatar
+                    large
+                    className="block h-16 w-16 origin-left"
+                    style={{transform: "var(--avatar-image-transform)"}}
+                  />
+                </div>
+              </div>
+            </Container>
+          </Fragment>
+        )}
+        <div
+          ref={headerRef}
+          className="top-0 z-10 h-16 py-6"
+          style={{position: "var(--header-position)" as CSSProperties["position"]}}>
+          <Container
+            className="top-[var(--header-top,theme(spacing.6))] w-full"
+            style={{position: "var(--header-inner-position)" as CSSProperties["position"]}}>
+            <div className="relative flex gap-4">
+              <div className="flex flex-1">
+                {!isHomePage && (
+                  <AvatarContainer>
+                    <Avatar />
+                  </AvatarContainer>
+                )}
+              </div>
+              <div className="flex flex-1 justify-end md:justify-center">
+                <MobileNavigation className="pointer-events-auto md:hidden" />
+                <DesktopNavigation className="pointer-events-auto hidden md:block" />
+              </div>
+              <div className="flex justify-end md:flex-1">
+                <div className="pointer-events-auto">
+                  <ThemeToggle />
+                </div>
               </div>
             </div>
           </Container>
-        </Fragment>
-      )}
-      <div
-        ref={headerRef}
-        className="top-0 z-10 h-16 py-6"
-        style={{position: "var(--header-position)" as CSSProperties["position"]}}>
-        <Container
-          className="top-[var(--header-top,theme(spacing.6))] w-full"
-          style={{position: "var(--header-inner-position)" as CSSProperties["position"]}}>
-          <div className="relative flex gap-4">
-            <div className="flex flex-1">
-              {!isHomePage && (
-                <AvatarContainer>
-                  <Avatar />
-                </AvatarContainer>
-              )}
-            </div>
-            <div className="flex flex-1 justify-end md:justify-center">
-              <MobileNavigation className="pointer-events-auto md:hidden" />
-              <DesktopNavigation className="pointer-events-auto hidden md:block" />
-            </div>
-            <div className="flex justify-end md:flex-1">
-              <div className="pointer-events-auto">
-                <ThemeToggle />
-              </div>
-            </div>
-          </div>
-        </Container>
-      </div>
-    </header>
+        </div>
+      </header>
+      {isHomePage && <div className="flex-none" style={{height: "var(--content-offset)"}} />}
+    </Fragment>
   )
 }
 
