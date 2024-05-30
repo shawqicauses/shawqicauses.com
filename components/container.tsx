6216413f4c3eb17cd62de4f70376c802d188cc87
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-30 23:53:46 +0200

    New: Enhance through adding `ContainerOuter` to `Container`

--- components/container.tsx
+++ components/container.tsx
@@ -1,4 +1,20 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../lib/utils"
+
+export const ContainerOuter = forwardRef<ElementRef<"div">, ComponentPropsWithoutRef<"div">>(
+  ({className, children, ...props}, ref) => {
+    return (
+      <div ref={ref} className={cn("sm:px-8", className)} {...props}>
+        <div className="mx-auto w-full max-w-xl-7 lg:px-8">{children}</div>
+      </div>
+    )
+  }
+)
+
+ContainerOuter.displayName = "ContainerOuter"
+
 const Container = function Container() {
   return <div>Container</div>
 }
